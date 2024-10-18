6a8a4bafef11c7ffd0031321bf9b48d376b9c3d5
Joao Victor <joao.victor.ssv@outlook.com>
2024-10-17 21:00:24 -0300

    feat: initial structure with tailwindcss.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sci2biz-client');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, sci2biz-client',
+    );
   });
 });
